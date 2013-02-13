c71efdbab943f8d905e6f9fe3cb7c73fc2d7c340
assgJones <bjones@allsouls.qld.edu.au>
2013-02-14 09:44:41 +1000

    Update sorting.py
    Added swap and make list functions

--- sorting.py
+++ sorting.py
@@ -1,2 +1,15 @@
+import random
+
 def swap(alist, leftIndex, rightIndex):
+    temp = alist[leftIndex]
+    alist[leftIndex] = alist[rightIndex]
+    alist[rightIndex] = temp
+    return alist
+
+def makeList(length):
+    answer = []
+    for i in range (length):
+        answer.append(random.randint(0,length)
+    return answer
+
     
